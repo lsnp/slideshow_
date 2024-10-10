f44134914e14823fe47e6e14858b25e825d7c0c7
lsx270 <qq_q@o2.pl>
2024-10-10 22:27:27 +0200

    fixed bisected paths issues, wc2

--- js/logo_jq1.js
+++ js/logo_jq1.js
@@ -3,7 +3,7 @@ $(function() {
 	// related examples and more: http://jquery.malsup.com/cycle/browser.html
 	
 	for (var i = 1; i <= 2; i++) {
-	  $('div').append('<img src="logo1.png"/>');
+	  $('div').append('<img src="../pic/logo1.png"/>');
 	}
 
 	$('.up').cycle({
